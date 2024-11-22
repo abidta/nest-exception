e4090f08133c63af8a70ca97071911cedc958eb5
abidta <sainulabidofficial@gmail.com>
2024-11-22 17:21:34 +0530

    added original source details in files

--- src/interfaces/http-exception-body.interface.ts
+++ src/interfaces/http-exception-body.interface.ts
@@ -1,3 +1,12 @@
+/*!
+ * Original Source: https://github.com/nestjs/nest
+ * Copyright (c) 2017-2024 Kamil Mysliwiec <https://kamilmysliwiec.com>
+ *
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ * See the LICENSE file in the root directory for more information.
+ *
+ */
+
 export type HttpExceptionBodyMessage = string | string[];
 
 export interface HttpExceptionBody {
